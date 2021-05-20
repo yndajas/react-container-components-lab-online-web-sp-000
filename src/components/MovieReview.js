6998eab8b3b843c282de34d765f38c9d933de989
Ynda Jas <yndajas@gmail.com>
2021-05-20 16:32:02 +0100

    Done.

--- src/components/MovieReview.js
+++ src/components/MovieReview.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MovieReview = ({imgSrc, title, summary, url, linkText}) => {
     return (
-        <div className="review">
+        <div>
             <img src={imgSrc} alt="movie image"/>
             <h3>{title}</h3>
             <p>"{summary}"</p>
